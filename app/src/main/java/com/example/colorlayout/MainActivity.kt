5e7a1ca151f82489550feae29cb04bf3c5647f66
da-nn-yy <db7394928@gmail.com>
2024-06-27 23:58:01 +0200

    modified:gradle.kts, AndroidMainfest.xml, MainActiviy.kt

--- app/src/main/java/com/example/colorlayout/MainActivity.kt
+++ app/src/main/java/com/example/colorlayout/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.colorlayout
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
